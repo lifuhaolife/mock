a5020679dd1d265a49449e37233a8c82ef703484
LFH <2448808186@qq.com>
2023-02-19 21:15:05 +0800

    【功能提交】 2.19 二进制中1的数量

---
+++ src/main/java/com/lfh/mock/ac/OneCount.java
@@ -0,0 +1,25 @@
+package com.lfh.mock.ac;
+
+import java.util.Scanner;
+
+public class OneCount {
+
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while(n -- != 0){
+            int x = sc.nextInt();
+            int res = 0;
+            while(x != 0){
+                x -= lowbit(x);
+                res ++;
+            }
+            System.out.print(res + " ");
+        }
+    }
+
+    public static int lowbit(int x){
+        return x & -x;
+    }
+}
