d589d9dfc64583a1ff990012a2f3cf885a2b65be
LFH <2448808186@qq.com>
2023-02-27 22:40:23 +0800

    【功能提交】 2.27 链表探测

--- src/main/java/com/lfh/mock/demo/DetectCycle.java
+++ src/main/java/com/lfh/mock/demo/DetectCycle.java
@@ -6,7 +6,7 @@ import java.util.Set;
 public class DetectCycle {
 
 
-    public ListNode detetCycle(ListNode head) {
+    public ListNode detectCycle(ListNode head) {
 
         Set<ListNode> listNodes = new HashSet<>();
 
@@ -15,12 +15,33 @@ public class DetectCycle {
         while (pos != null) {
             if (listNodes.contains(pos)) {
                 return pos;
-            }else {
+            } else {
                 listNodes.add(pos);
             }
             pos = pos.next;
         }
 
         return null;
+    }
+
+    public ListNode detectCycleTwoPointers(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+        while (head != slow) {
+            head = head.next;
+            slow = slow.next;
+        }
+        return head;
     }
 }
