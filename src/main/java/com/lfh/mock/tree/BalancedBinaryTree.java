2eb5676c9afef4d12460471443998dd29f523491
LFH <2448808186@qq.com>
2023-03-25 19:33:41 +0800

    【feature】3.25 BalancedBinaryTree

---
+++ src/main/java/com/lfh/mock/tree/BalancedBinaryTree.java
@@ -0,0 +1,30 @@
+package com.lfh.mock.tree;
+
+public class BalancedBinaryTree {
+
+
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(root.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+        int rightHeight = getHeight(root.right);
+        if (rightHeight == -1) {
+            return -1;
+        }
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+
+
+}
